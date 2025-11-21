12616bb7bfcd801aca59b246716a1eb2b90b69ef
jenilvaghasiya <vaghasiyajenil29@gmail.com>
2025-11-21 15:37:29 +0530

    Changes in this file index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,20 @@
-import { sequelize } from "../config/db.js";
+ import { sequelize } from "../config/db.js";
+ import Category from "./category.js";
+ import Product from "./product.js";
+ import User from "./user.js";
 
-const db = {};
-db.sequelize = sequelize;
+ const db = {};
+ db.sequelize = sequelize;
+ db.Category = Category;
+ db.Product = Product;
+ db.User = User;
 
-export default db;
+ // Associations
+ Category.hasMany(Product, { foreignKey: { name: "CategoryId", allowNull: false }, onDelete: "CASCADE" });
+ Product.belongsTo(Category, { foreignKey: { name: "CategoryId", allowNull: false } });
+
+ export const syncModels = async () => {
+   await sequelize.sync({ alter: true });
+ };
+
+ export default db;
